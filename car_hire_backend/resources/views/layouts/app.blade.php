fbdf6ad2c55a781e595304872fd949bbdfde4aa5
Orie Chinedu Emmanuel <oriechinedu@gmail.com>
2017-10-11 15:39:20 +0100

    add layouts and vendor

---
+++ car_hire_backend/resources/views/layouts/app.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="..Get Recent updates in tech indusrty">
+
+
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
+  <title>{{ config('app.name') }}</title>
+
+
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+  <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+  <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+  {{-- styles --}}
+  {{-- meta links peculiar to a page are yielded here --}}
+   @yield('meta-links')
+ 
+</head>
+<body>
+  <div id="app">
+      {{-- the header is yielded here --}}
+      @yield('header')
+
+      {{-- the main body content is yielded here--}}
+      @yield('content')
+
+      {{-- The footer is yielded here --}}
+      @yield('footer')
+  </div>
+
+    {{-- Js files links are yielded here --}}
+      @yield('js')
+</body>
+</html>
