fe5702d8f8a0b349cb313bae59d14a1ad57cb288
Ifunanya Ukachukwu <icukachuk@gmail.com>
2017-10-11 12:52:05 -0400

    locations

---
+++ Carhire-Locations.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Locations</title>
+	<link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300i,400,400i,700" rel="stylesheet"> 
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/Carhire-Locations.css">
+	<link rel="stylesheet" type="text/css" href="header1.php">
+
+</head>
+<body>
+	<?php include('header1.php') ?>
+	<div class = "container">
+		<div class = " world span12">
+			
+		</div>
+
+
+
+		<div class = "span 12 country_p">
+			<p>*pick your location to see our offices</p>
+			<p>You have selected Nigeria</p>
+			<br>
+
+		</div>
+
+		<div class="media">
+			<div class="media-left">
+				<img class="media-object" id = "ng-img" src="https://res.cloudinary.com/aiyfee/image/upload/v1507734166/Nigeria_i3lcxb.png" alt="Nigeria">
+			</div>
+			<div class="media-body">
+				<h1 class = "country media-heading">Nigeria</h1>
+			</div>
+		</div>
+
+
+	  	<div class = "container">
+	  		<div class = "row">
+	  			<div class = "col-md-2 states" >
+	  				  
+	  				<li> ABIA </li>
+	  				<li> ADAMAWA</li>
+	  				<li> AKWA IBOM</li>
+	  				<li> ANAMBRA</li>
+	  				<li> BAUCHI</li>
+	  				<li> BAYELSA</li>
+	  				
+	  			</div>
+	  			<div class = "col-md-2 states" >
+	  				     
+	  				<li> BENUE </li>
+	  				<li> BORNO</li>
+	  				<li> CROSS RIVER</li>
+	  				<li> DELTA</li>
+	  				<li> EBONYI </li>
+	  				<li> EDO</li>
+	  				
+	  			</div>
+	  			<div class = "col-md-2 states" >
+	  				         
+	  				<li> EKITI </li>
+	  				<li> ENUGU</li>
+	  				<li> GOMBE</li>
+	  				<li> IMO</li>
+	  				<li> JIGAWA </li>
+	  				<li> KADUNA</li>
+	  				
+	  			</div>
+	  			<div class = "col-md-2 states" >
+	  				              
+	  				<li> KANO </li>
+	  				<li> KATSINA</li>
+	  				<li> KEBBI</li>
+	  				<li> KOGI</li>
+	  				<li> KWARA </li>
+	  				<li style="border-bottom:3px solid green"> LAGOS</li>
+	  				
+	  			</div>
+	  			<div class = "col-md-2 states" >
+	  				                
+	  				<li> NASSARAWA </li>
+	  				<li> NIGER</li>
+	  				<li> OGUN</li>
+	  				<li> ONDO</li>
+	  				<li> OSUN  </li>
+	  				<li> OYO</li>
+	  				
+	  			</div>
+
+	  			<div class = "col-md-2 states" >
+	  				       
+       
+	  				<li> PLATEAU </li>
+					<li> RIVERS  </li>
+					<li> SOKOTO  </li>
+					<li> TARABA  </li>
+					<li> YOBE </li>
+					<li> ZAMFARA  </li>  
+	  				
+	  			</div>
+	  		</div>
+	  	</div>
+<br>
+	<div id="loc">
+    	
+        	<h2 style="color:green">Lagos</h2>
+      <div class="loczone">
+        <ul>
+        <li>Location details</li>
+        Address : Adeola Odeku street,  68A Lagos<br>
+        Location :Lagos city
+        GDS Code : Losco1
+        
+        </ul>
+        </div><!--ends loczone -->
+        
+         <div class="moreinfo">
+        <ul>
+        Opening Hours<br>
+        Mo. - Su  07:00 - 18:00<br>
+        Bank Holiday 07:00 - 18:00<br>
+                     24 hr pickup<br>
+                     24 hr return
+        
+        </ul>
+        </div><!--ends moreinfo -->
+        
+        
+        
+        	
+      <div class="loczone">
+        <ul>
+        <li>Location details</li>
+        Address : Old secreteriat Ikeja, GRA<br>
+        Location :Lagos city
+        GDS Code : Losco1
+        
+        </ul>
+        </div><!--ends loczone -->
+        
+         <div class="moreinfo">
+        <ul>
+        Opening Hours<br>
+        Mo. - Su.  07:00 - 18:00<br>
+        Bank Holiday 07:00 - 18:00<br>
+                     24 hr pickup<br>
+                     24 hr return
+        
+        </ul>
+        </div><!--ends moreinfo -->
+        
+        <div class="cl"></div>
+       
+    
+    </div><!--end loc-->
+
+	</div>
+		<div class="cat">
+    	<h3 style="color:blue; text-align:left">Categories and Services</h3>
+        <table >
+        <tr><td>Compact cars </td><td>4x4 / SUV</td><td>Truck</td><td>24 h pickup</td> <td>24 h return</td><td></td></tr>
+        </table>
+    </div>
+    <br>
+    <br>
+	<?php include('footer1.php') ?>
+
+	</div>
+
+<!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>    
+    <script src="bootstrap.min.js"></script>
+   
+</body>
+</html>
