b2a002c403b96ead2fa08ddc49b4969f91799b22
shield <myshield02@gmail.com>
2017-10-12 18:18:05 +0100

    car_hire_backend/database/migrations/2014_10_12_000000_create_users_table.php

--- car_hire_backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ car_hire_backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->increments('id')->index();
             $table->string('name');
             $table->string('email',100)->unique();
-            $table->string('password', 200);
+            $table->string('password');
             $table->tinyInteger('verified')->default(0);
             $table->string('role')->default(0);
             $table->string('email_token')->nullable();
