d3e31066827707582d09950c4fbab80b03d636e5
Orie Chinedu Emmanuel <oriechinedu@gmail.com>
2017-10-12 22:38:04 +0100

    fix merge conflict

--- car_hire_backend/app/Http/Controllers/CarController.php
+++ car_hire_backend/app/Http/Controllers/CarController.php
@@ -1,12 +1,9 @@
 <?php
 
-<<<<<<< HEAD
+
 namespace App\Http\Controllers;
  use App\Car;
-=======
-namespace App\Http\Controllers\Api\v1;
 
->>>>>>> b2a002c403b96ead2fa08ddc49b4969f91799b22
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -15,12 +12,7 @@ class CarController extends Controller
     /**
      * Display a listing of the resource.
      *
-<<<<<<< HEAD
-     * @return \Illuminate\Http\Response
-=======
-     * @return Response
->>>>>>> b2a002c403b96ead2fa08ddc49b4969f91799b22
-     */
+
     public function index()
     {
         //
@@ -29,25 +21,20 @@ class CarController extends Controller
     /**
      * Show the form for creating a new resource.
      *
-<<<<<<< HEAD
-     * @return \Illuminate\Http\Response
-=======
-     * @return Response
->>>>>>> b2a002c403b96ead2fa08ddc49b4969f91799b22
+
      */
     public function create()
     {
         //
-<<<<<<< HEAD
+
         return view('partials.add-car');
-=======
->>>>>>> b2a002c403b96ead2fa08ddc49b4969f91799b22
+
     }
 
     /**
      * Store a newly created resource in storage.
      *
-<<<<<<< HEAD
+
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
@@ -76,26 +63,21 @@ class CarController extends Controller
                 if ($car->save()){
             return redirect()->back()->with('data', 'Car instance added successfully');
         }
+    }
         
 
-=======
-     * @return Response
-     */
-    public function store()
-    {
-        //
->>>>>>> b2a002c403b96ead2fa08ddc49b4969f91799b22
-    }
+
+  
 
     /**
      * Display the specified resource.
      *
      * @param  int  $id
-<<<<<<< HEAD
+
      * @return \Illuminate\Http\Response
-=======
+
      * @return Response
->>>>>>> b2a002c403b96ead2fa08ddc49b4969f91799b22
+
      */
     public function show($id)
     {
@@ -106,11 +88,11 @@ class CarController extends Controller
      * Show the form for editing the specified resource.
      *
      * @param  int  $id
-<<<<<<< HEAD
+
      * @return \Illuminate\Http\Response
-=======
+
      * @return Response
->>>>>>> b2a002c403b96ead2fa08ddc49b4969f91799b22
+
      */
     public function edit($id)
     {
@@ -120,18 +102,18 @@ class CarController extends Controller
     /**
      * Update the specified resource in storage.
      *
-<<<<<<< HEAD
+
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
-=======
+
      * @param  int  $id
      * @return Response
      */
     public function update($id)
->>>>>>> b2a002c403b96ead2fa08ddc49b4969f91799b22
+
     {
         //
     }
@@ -140,17 +122,17 @@ class CarController extends Controller
      * Remove the specified resource from storage.
      *
      * @param  int  $id
-<<<<<<< HEAD
+
      * @return \Illuminate\Http\Response
-=======
+
      * @return Response
->>>>>>> b2a002c403b96ead2fa08ddc49b4969f91799b22
+
      */
     public function destroy($id)
     {
         //
     }
-<<<<<<< HEAD
+
 
     public function rules(){
           return [
@@ -166,6 +148,6 @@ class CarController extends Controller
     {
         return  md5(uniqid(rand(0, 1000), TRUE))."-".date('d-m-Y-H-i-s', time()).'.jpg';
     }
-=======
->>>>>>> b2a002c403b96ead2fa08ddc49b4969f91799b22
+
+
 }
