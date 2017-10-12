bf96abdfadc9f34978e64b9690f09ee603924388
matilbella <dikethelma55@gmail.com>
2017-10-12 14:26:59 +0100

    My site ready for deployment.

---
+++ car_hire_backend/resources/views/error/404.blade.php
@@ -0,0 +1,297 @@
+<!Doctype html>
+<html>
+<head>
+
+	<title>Error Page Car Hire</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+<style type="text/css">
+
+body{
+	background-color: #fff;
+	margin:0 auto;
+	font-family: sans-serif;
+}
+
+#list{
+	width: 100%;
+	height: 70px;
+	background-color: #0099cc;
+	overflow: hidden;
+}
+
+.menus{
+	float:right;
+	margin-top: -10px;
+}
+
+a{
+	text-decoration: none;
+}
+.error1{
+	padding: 10px;
+}
+
+.error2{
+	position: relative;
+	top: -57px;
+}
+
+.error3{
+	padding: 10px;
+}
+
+#aaa{
+	color:#242020;
+	font-weight: bold;
+	font-size: 16px;
+	
+
+}
+
+.aa{
+	color:white;
+	font-size: 18px;
+	font-weight: bold;
+}
+.aa:hover{
+	display: block;
+	font-size: 30px;
+	color:#000;
+	border-bottom: 4px solid orange;
+	padding-bottom: 14px;
+
+
+}
+
+#aaa:hover{
+	color: #0099cc;
+	border-bottom: 4px solid #000;
+	padding-bottom: 14px;
+}
+
+li{
+	display: inline-block;
+	padding: 10px;
+	margin-right: 15px;
+
+}
+
+h3{
+	text-align: center;
+	font-size: 17px;
+	color: darkblue;
+}
+
+#logo{
+	float:left;
+	padding-top: 20px;
+	margin-left: 50px;
+}
+h1{
+	text-align: center;
+	color: #0099cc;
+	padding-top: 40px;
+	font-size: 40px;
+	font-weight: bold;
+}
+#error{
+	padding-top: 60px;
+	margin-left: 460px;
+}
+#error:nth-child(2){
+	padding-top: -12px!important;
+}
+
+h2{
+	text-align: center;
+	font-weight: normal;
+	font-size: 20px;
+	color: gray;
+}
+
+button{
+    background-color: #0099cc;
+    border: solid;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 20px;
+}
+.button{
+	margin-left: 610px;
+}
+hr{
+	border: 2px solid gray;
+	margin-top: 25px;
+	margin-bottom: 25px;
+}
+
+.foot_list{
+
+}
+.list_color{
+	color: #000;
+	margin-left: -40px;
+}
+.list_color:hover{
+	text-decoration: none;
+	color: blue;
+}
+.container{
+	display: inline;
+	background-color:#fafaef;
+
+}
+.col-4{
+	float: left;
+	margin-left: 200px;
+	margin-top: 50px;
+}
+
+h4{
+	color: #000;
+	font-weight: bold;
+	font-size: 20px;
+}
+
+.div_footer{
+	width: 100%;
+	height: 40px;
+	background-color: #0099cc;
+}
+
+.footer{
+	text-align: center;
+	color: #fff;
+	padding: 10px;
+	font-weight: bold;
+}
+
+.row{
+	width: 100%;
+	height: 350px;
+	background-color: #fafaef;
+}
+
+
+
+</style>
+</head>
+
+<body>
+
+<div id="list">
+		<nav class="menus">
+			<ul>
+				<li><a class="aa" href="#"> Home </a></li>
+				<li><a class="aa" href="#"> Locations </a></li>
+				<li><a class="aa" href="#"> Special Offers</a></li>
+				<li><a class="aa" href="#"> Cars</a></li>
+				<li><a class="aa" href="#">Contact</a></li>
+				<li><a class="aa" href="#">Airport Transfer</a></li>
+
+			</ul>
+
+		</nav>
+
+	</div>
+	<div style="clear:both">
+	</div>
+
+	<div id="logo">
+		<img src="http://res.cloudinary.com/siazo/image/upload/v1507562729/car_jyrdnk.png"height="70" width="145">
+	</div>
+
+	<div style="float:right;">
+		<nav id="second_menu">
+			<ul>
+				<li><a id="aaa" href="#"> RENT A CAR </a></li>||
+				<li><a id="aaa" href="#"> NEW CARS </a></li>
+		
+			</ul>
+
+		</nav>
+	</div>
+		<div style="clear:both">
+
+		<h1>ARRGH!!</h1>
+	<div id="error">
+		<img class="error1" src="http://res.cloudinary.com/heptapixels/image/upload/v1507669524/Vector_2_hm4p00.svg">
+		<img class="error2"src="http://res.cloudinary.com/heptapixels/image/upload/v1507669509/Vector_1_bgmoyk.svg">
+		<img class="error3"src="http://res.cloudinary.com/heptapixels/image/upload/v1507669524/Vector_2_hm4p00.svg">
+	</div>
+		<h2>Sorry the page you are looking for cannot be accessed. <br>check the options below or feel free to report to!</h2>
+
+		<h3>REPORT THIS ISSUE</h3>
+
+	<div class="button">
+		<button>HOME</button>
+	</div>
+
+
+	<hr>
+
+	<div style="clear:both">
+	<div class="container">
+		<div class="row">
+
+			<div class="col-5">
+				<div id="logo">
+					<img src="http://res.cloudinary.com/siazo/image/upload/v1507562729/car_jyrdnk.png"height="70" width="145">
+					<ul class="foot_list">
+						<p><li><a class="list_color" href="#">About</a></li></p>
+						<p><li><a class="list_color" href="#">Locations</a></li></p>
+						<p><li><a class="list_color" href="#">Contact Us</a></li></p>
+						<p><li><a class="list_color" href="#">Privacy Policy</a></li></p>
+					</ul>
+				</div>
+				<div class="col-4">
+				<h4>Product & Services</h4>
+				<ul class="foot_list">
+						<p><li><a class="list_color" href="#">Vehicles</a></li></p>
+						<p><li><a class="list_color" href="#">Drivers</a></li></p>
+						<p><li><a class="list_color" href="#">Hire Advice</a></li></p>
+						<p><li><a class="list_color" href="#">FAQs</a></li></p>
+				</ul>
+			</div>
+			
+			<div class="col-4">
+				<h4>Our Partner</h4>
+					<ul class="foot_list">
+						<p><li><a class="list_color" href="#">Hotels.ng</a></li></p>
+						<p><li><a class="list_color" href="#">FAAN</a></li></p>
+						
+					</ul>
+			</div>
+
+			<div class="col-4">
+				<h4>follow Us</h4>
+				
+				<a href="www.twitter.com"><img src="http://res.cloudinary.com/siazo/image/upload/v1507639912/twitter-256_z1pfxb.png" height="40" width="40"></a>
+    			
+    			<a href="www.linkedin.com"><img src="https://res.cloudinary.com/siazo/image/upload/v1507639847/linkedin_circle-512_hzqz6w.png"  height="40" width="40" id="icon"></a>
+    			
+    			<a href="www.youtube.com"><img src="https://res.cloudinary.com/siazo/image/upload/v1507639699/youtube_gm3o65.svg"  height="40" width="40" id="icon"></a>
+
+    		</div>
+			</div>
+		</div>
+	</div>
+			
+			<div style="clear:both">
+
+    		<div class="div_footer">
+    		<footer>
+    			<div class="footer">&copy; 2017-CarHire-All Rights Reserved </div>
+			</footer>
+			</div>
+
+		</div>
+	</div>
+</body>
+</html>
