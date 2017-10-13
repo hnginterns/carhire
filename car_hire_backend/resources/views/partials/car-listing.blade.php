a73a0c744f47ebc8d52c4e91bb5d92ffae90ec8b
Orie Chinedu Emmanuel <oriechinedu@gmail.com>
2017-10-13 13:25:59 +0100

    converted carlisting to blade

--- car_hire_backend/resources/views/partials/car-listing.blade.php
+++ car_hire_backend/resources/views/partials/car-listing.blade.php
@@ -1,12 +1,108 @@
- @extends('layouts.app')
-  @section('meta-links')
-     <link rel="stylesheet" type="text/css" href="{{asset('css/carlisting.css')}}">
-  @endsection
-    @section('header')
-    @include('partials.header')
-   @endsection
-@section('content')
-<div class="headcontent">
+<!Doctype html>
+<html>
+<head>
+
+  <title>Car Listing</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="{{asset('css/header.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('css/carlisting.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('css/footer.css')}}">
+
+    <link rel="stylesheet" media="only screen and (min-width: 320px)" href="css/carlisting2.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    
+</head>
+<style>
+@media only screen and (min-width: 450px) {
+      .first, .second, ul{
+        float:left;
+        width:50%;
+        margin:0;
+    }
+
+ .reserve{
+    margin-top: -.6%;
+    margin-left:3%;
+}
+#price{
+    margin-top:2%;
+}
+.head1 p {
+    text-align:center;
+}
+}
+@media only screen and (min-width: 720px) {
+.rebox1, .rebox2, .rebox3, .rebox4{
+    width:24%;
+}
+.head2{
+    margin-left: 3%;
+}
+.rebox1 img{
+    margin-top: 13%;
+}
+.rebox1 p{
+    margin-top:-8%;
+}
+.rebox2 img{
+    margin-top: 20%;
+}
+.rebox3 img{
+    margin-top: 20%;
+}
+.rebox4 img{
+    margin-top: 20%;
+}
+ .car img{
+    width: 50%;
+    height: 50%;
+}
+.text{
+    text-align: left;
+    margin-right:13%;
+    float:right;
+    margin-top:-23%;
+}
+#price p .prices{
+    float:left;
+    margin-top:1%;
+    font-weight: lighter;
+    margin-left:30%;
+    color:#007BCA;
+    font-size: .8em;
+    line-height: 2em;
+}
+#price p .NGN{
+    margin-left:-30%;
+}
+.box{
+    font-size:1em;
+}
+}
+
+
+@media only screen and (min-width: 1100px) {
+    body{
+      font-weight:700;
+    font-family: 'Roboto', sans-serif;
+}
+p{
+    margin: 0;
+}
+}
+
+</style>
+   
+<body>
+
+
+    <header>
+          @include('partials.header') 
+  </header>
+    
+
+       <div class="headcontent">
         <div class="head1">
             <p>Vehicle Models</p>
             <form>
@@ -22,23 +118,24 @@
                     <option value="red">Nairobi</option>
                 </select>
             </form>
+                <p class="clear" />
         </div>
         <div class="head2">
             <p>4 of 8 available vehicle groups are currently shown</p><br>
             <div class="rebox1">
-                <img src="1.png" alt="door">
+                <img src="{{asset('1.png')}}" alt="door">
                 <p> Models </p>
             </div>
             <div class="rebox2">
-                  <img src="2.png" alt="door">
+                  <img src="{{asset('images/2.png')}}" alt="door">
                 <p> Doors</p>
             </div>
             <div class="rebox3">
-                  <img src="3.png" alt="door">
+                  <img src="{{asset('images/3.png')}}" alt="door">
                 <p> Passenger </p>
             </div>
             <div class="rebox4">
-                  <img src="4.png" alt="door">
+                  <img src="{{asset('images/4.png')}}" alt="door">
                 <p> Transmission</p>
             </div>
         </div>
@@ -50,11 +147,11 @@
 <!--subcontainer div to house car, car name and price-->
         <div class="car">
 
-        <img src="car.png" alt="car" >
-        <span class="text">
+        <img src="{{asset('images/car.png')}}" alt="car" >
+        <div class="text">
               <h3> Car Name | ECAR</h3>
               <h3>Hyundai Accent, Chevrolet Spark</h3>
-        </span>
+        </div>
       
             <p class="clear" />
 
@@ -100,58 +197,7 @@
 <!--subcontainer div to house car, car name and price-->
         <div class="car">
 
-        <img src="car.png" alt="car" >
-        <span class="text">
-              <h3> Car Name | ECAR</h3>
-              <h3>Hyundai Accent, Chevrolet Spark</h3>
-        </span>
-      
-            <p class="clear" />
-
-            <div id="price"> 
-                <p>
-                    <span class="prices">prices per day</span> 
-                    <span class="NGN">NGN</span> 
-                    <span class="number">499.26 </span> 
-                    </p>
-             </div>
-         </div>
-    
-<!--subcontainer div to house car details and reserve-->
-        <div class="car-details">
-            <div class="first">
-                <ul >
-                    <li><span>5 Airconditioners</span></li>
-                    <li><span>4 Doors</span></li>
-                    <li><span>1 Air Bag</span></li>
-                    <li><span>5 Seats</span></li>
-                </ul>
-            </div>
-        
-            <div  class="second">
-            <ul>
-                <li><span>5 Children</span></li>
-                <li><span>5 Adults</span></li>
-                <li><span>5 Suitcases</span></li>
-                <li><span>5 Leg Room</span></li>
-           </ul>
-           </div>
-        
-        <p class="clear" />
-            <div class="reserve">
-                <p>RESERVE NOW</p>
-            </div>
-        </div>
-        <p class="clear" />
-      </div>
-      <!--container div for each car ends here-->
-
-      <!--container div for each car starts here-->
-      <div class="box">
-<!--subcontainer div to house car, car name and price-->
-        <div class="car">
-
-        <img src="car.png" alt="car" >
+        <img src="{{asset('images/car.png')}}" alt="car" >
         <span class="text">
               <h3> Car Name | ECAR</h3>
               <h3>Hyundai Accent, Chevrolet Spark</h3>
@@ -202,7 +248,7 @@
 <!--subcontainer div to house car, car name and price-->
         <div class="car">
 
-        <img src="car.png" alt="car" >
+        <img src="{{asset('images/car.png')}}" alt="car" >
         <span class="text">
               <h3> Car Name | ECAR</h3>
               <h3>Hyundai Accent, Chevrolet Spark</h3>
@@ -244,8 +290,64 @@
                 <p>RESERVE NOW</p>
             </div>
         </div>
-       @endsection
+        <p class="clear" />
+      </div>
+      <!--container div for each car ends here-->
 
-      @section('footer')
-      @include('partials.footer')
-      @endsection
+      <!--container div for each car starts here-->
+      <div class="box">
+<!--subcontainer div to house car, car name and price-->
+        <div class="car">
+
+        <img src="{{asset('images/car.png')}}" alt="car" >
+        <span class="text">
+              <h3> Car Name | ECAR</h3>
+              <h3>Hyundai Accent, Chevrolet Spark</h3>
+        </span>
+      
+            <p class="clear" />
+
+            <div id="price"> 
+                <p>
+                    <span class="prices">prices per day</span> 
+                    <span class="NGN">NGN</span> 
+                    <span class="number">499.26 </span> 
+                    </p>
+             </div>
+         </div>
+    
+<!--subcontainer div to house car details and reserve-->
+        <div class="car-details">
+            <div class="first">
+                <ul >
+                    <li><span>5 Airconditioners</span></li>
+                    <li><span>4 Doors</span></li>
+                    <li><span>1 Air Bag</span></li>
+                    <li><span>5 Seats</span></li>
+                </ul>
+            </div>
+        
+            <div  class="second">
+            <ul>
+                <li><span>5 Children</span></li>
+                <li><span>5 Adults</span></li>
+                <li><span>5 Suitcases</span></li>
+                <li><span>5 Leg Room</span></li>
+           </ul>
+           </div>
+        
+        <p class="clear" />
+            <div class="reserve">
+                <p>RESERVE NOW</p>
+            </div>
+        </div>
+        <p class="clear" />
+      </div>
+      <!--container div for each car ends here-->
+
+ <footer>
+    @include('partials.footer')
+</footer>
+
+</body>
+</html>
--- car_hire_backend/resources/views/partials/header.blade.php
+++ car_hire_backend/resources/views/partials/header.blade.php
@@ -1,52 +1,44 @@
+<!-- FIRST NAV BAR -->
+  <nav class="navbar navbar-default header-blue" role="navigation">
+    <div class="container">
+  
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse navbar-ex1-collapse">
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="/">Home</a></li>
+          <li><a href="#locations">Location</a></li>
+          <li><a href="#specialoffers">Speecial Offers</a></li>
+          <li><a href="car-listing">Cars</a></li>
+          <li><a href="/contact">Contact</a></li>
+          <!-- <li><a href="#">Airport Transfer</a></li> -->
+        </ul>
+      </div><!-- /.navbar-collapse -->
+    </div>
+  </nav>
+  <!-- END FIRST NAV BAR -->
 
 
-{{--   <link rel="stylesheet" href="css/main.css" --}}
-
-  <header>
-	<nav class="navbar navbar-inverse ">
-  <div class="container">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-    toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-        
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
+  <!-- SECOND NAV BAR -->
+  <nav class="navbar navbar-default header-below" role="navigation">
+    <div class="container">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-left" href="#"><a href="#"><img class="img-responsive" src="http://res.cloudinary.com/siazo/image/upload/v1507562729/car_jyrdnk.png" height="80px"></a></a>
+      </div>
+  
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse navbar-ex1-collapse">
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="/car-listing">FIND A CAR</a></li>
+          <li class="pipe hidden-xs"><a href="#">|</a></li>
+          <li><a href="car-listing">NEW CARS</a></li>
+        </ul>
+      </div><!-- /.navbar-collapse -->
     </div>
-    <div id="navbar" class="navbar-collapse collapse">
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">Home</a></li>
-        <li><a href="#about">Location</a></li>
-        <li><a href="#">Special Offers</a></li>  
-        <li><a href="/car-listing">Cars</a></li>  
-        <li><a href="/contact">Contact</a></li>  
-        <li><a href="#">Airport Transfer</a></li>   
-      </ul>
-    </div>
-  </div>
-  		<!--second nav-->
-  		<!--this nav is described with the center attribute-->
-</nav>
-<nav class="navbar navbar-inverse nav2" 
-id="nav2">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span> 
-      </button>
-      
-    </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
-       <a class="navbar-brand image" href="#"><img class="center image" src="http://res.cloudinary.com/siazo/image/upload/v1507562729/car_jyrdnk.png" height="50" width="158"></a>
-      <ul class="nav navbar-nav navbar-right" id="center">
-        <li><a href="#" class="center rent">Rent a car</a></li>
-      <li><a href="#" class="center line ">|</a></li>
-      <li><a href="#" class="center new">New cars</a></li>
-      
-      </ul>
-    </div>
-  </div>
-</nav>
-</header>
+  </nav>
