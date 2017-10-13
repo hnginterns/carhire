7d019aa0f60f15307103c3b7d9244a2930ccf30b
shield <myshield02@gmail.com>
2017-10-13 16:01:31 +0100

    booking blade

---
+++ car_hire_backend/resources/views/partials/bookings.blade.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Congratulation</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+<header>
+      @include('partials.header')
+</header>
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
+      <div class="col-md-4 ">
+        <p>Welcome, Kingsley Ukeje</p>
+        <img src="http://res.cloudinary.com/dawclpl72/image/upload/v1507724154/vlad-sorodoc-49620_mokx9a.png" class="img-responsive" alt="car">
+        <p class="carname">Ford Figo with Drive</p>
+        <p>PICK UP</p>
+        <h5>Ibom Elibrary,Uyo</h5>
+        <h5 class="info">13-10-2017 at 23:05</h5>
+
+        <p style"padding:auto">RETURN</p>
+        <h5>Ibom Elibrary,Uyo</h5>
+        <h5 class="info">13-10-2017 at 23:05</h5>
+
+        <p style"padding:auto">RENTAL</p>
+        <h5 class="info">1 Day</h5>
+        
+        <p style"padding:auto">Options</p>
+        <h5>Air Conditioner</h5>
+        <h5>Automatic Transmission</h5>
+        <h5 class="info">5 Doors</h5>
+        
+        <h5>Minimum Age for Drivesr</h5>
+        <h5 class="info">18year</h5>
+
+        <button type="button" class="btn btn-primary">View Full Details</button>
+      </div>
+   
+              
+        <div class="col-md-8 ">
+          <h3 class="h3text">Congratulations!, Well rented</h3>
+      
+          <div class="well congrat">
+            
+              <div class="row book" >
+              <p >You Reservation Has been Received by Us</p>
+              <p>You will Recieve a Confirmation Email From Us</p><br><br>
+                <div class="col-md-6 col-xs-8">
+                    <strong><p>Your Reservation Number: </p></strong>
+                    <strong> <p>Your Total Rental Price: </p></strong>
+                </div>
+                <div class="col-md-6  col-xs-4 ">
+                  <strong><p>344-564-675</p></strong>
+                  <strong> <p>$35 </p></strong>
+                </div>
+            </div><br>
+              <p>We Wish You a Good Trip</p>
+      
+          </div>
+         
+          <div class="well  passbook">
+            <p> <span class="fa fa-play" ></span>&nbsp &nbsp Passbook</p>
+            <p> <span class="fa fa-play"></span>&nbsp &nbsp Download Reservation Confirmation</p>
+            <br><br><br>
+      
+            <p>Important Information About Vehicle Collection</p>
+          </div>
+        </div>	
+   
+   </div>
+  </div>
+</div>    
+<footer>
+    @include('partials.footer')
+</footer> 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" ></script>
+<script src="https://use.fontawesome.com/0faa1f6bdb.js"></script>
+</body>
+</html>
