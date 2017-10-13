348bb47589cb624af915b262bb3ca509542d4f70
Oluwabukola Tina <oluwabukolatina2015@gmail.com>
2017-10-13 11:06:39 +0100

    linked the pages

--- index.php
+++ index.php
@@ -205,9 +205,9 @@ id="nav2">
                 <div class="col-sm-3">
                     <h2 class="logo"><a href="#"><img class="img-responsive" src="http://res.cloudinary.com/siazo/image/upload/v1507562729/car_jyrdnk.png" width="100px"></a></h2>
                     <ul>
-                        <li><a href="#">About</a></li>
-                        <li><a href="#">Locations</a></li>
-                        <li><a href="#">Contact Us</a></li>
+                        <li><a href="aboutus.php">About</a></li>
+                        <li><a href="locations.php">Locations</a></li>
+                        <li><a href="contactpage.php">Contact Us</a></li>
                         <li><a href="#">Privacy Policy</a></li>
                     </ul>
                 </div>
@@ -215,16 +215,16 @@ id="nav2">
                     <h5>Products & Services</h5>
                     <ul>
                         <li><a href="#">Vehichles</a></li>
-                        <li><a href="#">Drivers</a></li>
+                        <li><a href="driver-info.php">Drivers</a></li>
                         <li><a href="#">Hire Advice</a></li>
-                        <li><a href="#">FAQs</a></li>
+                        <li><a href="faq.php">FAQs</a></li>
                     </ul>
                 </div>
                 <div class="col-sm-3">
                     <h5>Our Partners</h5>
                     <ul>
-                        <li><a href="#">Hotels.ng</a></li>
-                        <li><a href="#">FAAN</a></li>
+                        <li><a href="https://hotels.ng">Hotels.ng</a></li>
+                        <li><a href="https://http://www.faan.gov.ng">FAAN</a></li>
                     </ul>
                 </div>
                 <div class="col-sm-3">
